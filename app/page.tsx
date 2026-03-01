206cbcc29437f5e52d0f1557e325e14c21c51b8f
Shubham Midgule <shubham.midgule17@gmail.com>
2026-03-01 11:55:22 +0530

    Add BookCard and HeroSection components; implement sample book display and constants

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
+import BookCard from "@/components/BookCard";
+import HeroSection from "@/components/HeroSection";
+import { sampleBooks } from "@/lib/constants";
+
 const page = () => {
   return (
-    <div>
-      <h1 className='text-4xl font-extrabold'>Bookify</h1>
-    </div>
-  )
-}
+    <main className="wrapper container">
+      <HeroSection />
 
-export default page
+      <div className="library-books-grid">
+        {sampleBooks.map((book) => (
+          <BookCard
+            key={book._id}
+            title={book.title}
+            author={book.author}
+            coverURL={book.coverURL}
+            slug={book.slug}
+          />
+        ))}
+      </div>
+    </main>
+  );
+};
+
+export default page;
